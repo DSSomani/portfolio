f3f5417f728d3af7ad92d95f8c3883d405dd861f
Dharmik Somani <129661182+DSSomani@users.noreply.github.com>
2023-07-21 00:22:12 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-let menu = document.querySelector('#manu-icon');
-let navbar = document.querySelector('.navItems');
-
-menu.onclick = () => {
-    menu.classList.toggle('fa-solid fa-xmark');
-    navbar.classList.toggle('open');
-}
