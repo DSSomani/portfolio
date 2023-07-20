1e10ece08dfd8d364d689b52456eb215677a8e34
Dharmik Somani <129661182+DSSomani@users.noreply.github.com>
2023-07-21 00:34:27 +0530

    Delete portfolio directory

--- portfolio/script.js
+++
@@ -1,9 +0,0 @@
-var menu = document.getElementById('manu-icon');
-let navbar = document.querySelector('.navItems');
-
-
-menu.onclick = function() {
-  menu.classList.toggle('fa-xmark');
-  
-  navbar.classList.toggle('open');
-};
