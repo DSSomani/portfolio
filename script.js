29042732303d95f485a483b3f1749584ebca814f
dharmiksomani51 <dharmiksomani51@gmail.com>
2025-10-20 02:03:35 +0530

    i don't know what did i do.

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+gsap.registerPlugin(SplitText, ScrollTrigger);
+
+console.clear();
+
+gsap.set(".split", { opacity: 1 });
 var menu = document.getElementById('manu-icon');
 let navbar = document.querySelector('.navItems');
 
@@ -7,3 +12,33 @@ menu.onclick = function() {
   
   navbar.classList.toggle('open');
 };
+
+document.fonts.ready.then(() => {
+  let containers = gsap.utils.toArray(".column");
+
+  containers.forEach((container) => {
+    let text = container.querySelector(".split");
+    let animation;
+
+    SplitText.create(text, {
+      type: "lines, words",
+      mask: "lines",
+      linesClass: "line",
+      // autoSplit: true,
+      onSplit: (instance) => {
+        console.log("split")
+        return gsap.from(instance.lines, {
+          yPercent: 120,
+          stagger: 0.1,
+          scrollTrigger: {
+            trigger: container,
+            markers: true,
+            scrub: true,
+            start: "clamp(top center)",
+            end: "clamp(bottom center)"
+          }
+        });
+      }
+    });
+  });
+});
